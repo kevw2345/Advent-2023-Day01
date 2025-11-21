babcb37115813e6cfc840eb3ec9a7bb21c38b4c4
Kev Wong <kevlynw@nycstudents.net>
2025-11-21 09:53:58 -0500

    part 2 almost solved adshfoiuyaes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -23,17 +24,71 @@ public class Main {
     }
 
     public static int getPartOneNumber(String line) {
-        // do part 1
-        // find the first digit and last digit of the String
-        // return the number put together
-        return 0;
+        int i = 0;
+        char dig1 = ' ';
+        char dig2 = ' ';
+        while (dig2 == ' ' || dig1 == ' ') {
+            if (dig1 == ' ' && Character.isDigit(line.charAt(i))) {
+                dig1 = line.charAt(i);
+            }
+            char backChar = line.charAt(line.length() - 1 - i);
+            if (dig2 == ' ' && Character.isDigit(backChar)) {
+                dig2 = backChar;
+            }
+            i++;
+        }
+        String stringNum = Character.toString(dig1) + Character.toString(dig2);
+        System.out.println(stringNum);
+        return Integer.parseInt(stringNum);
     }
 
     public static int getPartTwoNumber(String line) {
-        // do part 2
-        return 0;
+        int i = 0;
+        char dig1 = ' ';
+        char dig2 = ' ';
+        while (dig2 == ' ' || dig1 == ' ') {
+            if (dig1 == ' ') {
+                if (Character.isDigit(line.charAt(i))) {
+                    dig1 = line.charAt(i);
+                } else if (checkForTextNumber(line, line.charAt(i)) != -1) {
+                    dig1 = (char)(checkForTextNumber(line, line.charAt(i)));
+                }
+            }
+            char backChar = line.charAt(line.length() - 1 - i);
+            if (dig2 == ' ') {
+                if (Character.isDigit(backChar)) {
+                    dig2 = backChar;
+                } else if (checkForTextNumber(line, backChar) != -1) {
+                    dig2 = (char)(checkForTextNumber(line, backChar));
+                }
+            }
+            i++;
+        }
+        String stringNum = Character.toString(dig1) + Character.toString(dig2);
+        System.out.println(stringNum);
+        return Integer.parseInt(stringNum);
     }
 
+    public static int checkForTextNumber(String line, int idx) {
+        HashMap<String, Integer> textNumbers = new HashMap<>();
+        textNumbers.put("zero", 0);
+        textNumbers.put("one", 1);
+        textNumbers.put("two", 2);
+        textNumbers.put("three", 3);
+        textNumbers.put("four", 4);
+        textNumbers.put("five", 5);
+        textNumbers.put("six", 6);
+        textNumbers.put("seven", 7);
+        textNumbers.put("eight", 8);
+        textNumbers.put("nine", 9);
+        for (String key : textNumbers.keySet()) {
+            if (idx + key.length() <= line.length() && line.substring(idx, idx + key.length()).equals(key)) {
+                return textNumbers.get(key);
+            }
+        }
+        //number not found
+        return -1;
+    }
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
         try {
@@ -50,4 +105,5 @@ public class Main {
             return fileData;
         }
     }
+
 }
